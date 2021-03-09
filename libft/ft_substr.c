a67b6cd67223e284f51fc5ad316f9d0328d202c4
Teo THIBAUT <tthibaut@e2r3p5.42.fr>
2021-03-09 12:53:47 +0100

    substr fini

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tthibaut <tthibaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 18:00:21 by tthibaut          #+#    #+#             */
-/*   Updated: 2021/03/06 14:57:54 by tthibaut         ###   ########.fr       */
+/*   Updated: 2021/03/09 12:46:52 by tthibaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
+	str = 0;
 	i = 0;
 	j = start;
-	while (s[j] && i < len)
-	{
-		j++;
-		i++;
-	}
+	if (s == NULL)
+		return (0);
+	if (start < ft_strlen(s))
+		while (s[j++] && i < len)
+			i++;
 	j = 0;
 	if (!(str = malloc(sizeof(char) * (i + 1))))
 		return (0);
