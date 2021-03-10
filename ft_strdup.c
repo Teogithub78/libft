d5c595df34cc28e586d97885dadfe744432d63a5
Teo THIBAUT <tthibaut@e2r3p5.42.fr>
2021-03-10 18:06:36 +0100

    clean and final

---
+++ ft_strdup.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tthibaut <tthibaut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/25 17:22:11 by tthibaut          #+#    #+#             */
+/*   Updated: 2021/03/06 15:18:45 by tthibaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		i++;
+	}
+	if (!(str = malloc(sizeof(char) * (i + 1))))
+		return (0);
+	while (j < i)
+	{
+		str[j] = s1[j];
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
